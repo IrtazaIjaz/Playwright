2f1acda6d0bafec06081620ed853702f7b33d455
Irtaza Ijaz <iijaz01@i2c.com>
2025-09-17 22:07:07 +0500

    Updated tests and fixes 09/17/2025 V4

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,7 +1,10 @@
 from pages.google_page import GooglePage
+from playwright.sync_api import expect
 
 def test_google_search(page):
     google = GooglePage(page)
     google.goto()
     google.search("Playwright Python")
-    assert "playwright" in page.title().lower()
+
+    # Wait for search results
+    expect(page).to_have_title(lambda title: "playwright" in title.lower())
